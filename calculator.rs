6accc7410fe9c2330ce34458c3ee850f671bb6b0
Raylee <abramscarter1999@gmail.com>
2026-07-29 12:33:31 -0400

    Update comment format in calculator.rs

--- calculator.rs
+++ calculator.rs
@@ -6,12 +6,12 @@ use std::process;
 //  setting up functions 
 /////////////////////    /////////////////////
 
-//check if inputted character is an operation
+// check if inputted character is an operation
 fn is_op(ch: char) -> bool {
     return ['+', '-', '*', '/'].contains(&ch);
 }
 
-//run operation based on the provided symbol
+// run operation based on the provided symbol
 macro_rules! run_operation {
     ($stack:ident, $op:tt) => {{
         $stack.push(&$stack[$stack.len() - 2] $op &$stack[$stack.len() - 1]);
@@ -20,7 +20,7 @@ macro_rules! run_operation {
     }};
 }
 
-//print out error message and set error to true
+// print out error message and set error to true
 fn error_occurred(err_type: &str, err_message: &str, err: &mut bool) {
     println!("{err_type} ERROR: {err_message}");
     *err = true;
@@ -46,18 +46,18 @@ fn main() {
 
         print!("> ");
 
-        //getting acutal input
+        // getting acutal input
         io::stdout().flush().unwrap();
         let mut input_equation = String::new();
         
-        //reading and formatting input
+        // reading and formatting input
         io::stdin().read_line(&mut input_equation).expect("error reading user input line");
         let equation = input_equation.trim();
 
-        //turning equation into equation_list
+        // turning equation into equation_list
         let mut equation_list: Vec<char> = equation.chars().collect();
         
-        //add empty char at the end to deal with some edge cases
+        // add empty char at the end to deal with some edge cases
         equation_list.push(' ');
 
         /////////////////////    /////////////////////
@@ -80,19 +80,19 @@ fn main() {
             //  checking if negative then checking for operation
             /////////////////////    /////////////////////
 
-            //checking if negative
+            // checking if negative
             if ind < equation_list.len() {
                 if character == '-' && equation_list[ind+1].is_digit(10) && num_build_list.is_empty() {
                     negative = true;
                 }
             }
             
-            //checking for operation
+            // checking for operation
             if is_op(character) && equation_list[ind+1] == ' ' {
                 operation = character;
             }
 
-            //syntax error if there is a misplaced operation
+            // syntax error if there is a misplaced operation
             if is_op(character) && stack.len() < 2 && negative == false {
                 error_occurred("SYNTAX", "incorrect operation placement", &mut error);
                 break;
@@ -102,45 +102,45 @@ fn main() {
             //  parse number input
             /////////////////////    /////////////////////
 
-            //syntax error if invalid character is found
+            // syntax error if invalid character is found
             if !character.is_digit(10) && !is_op(character) && character != ' ' && character != '.' {
                 error_occurred("SYNTAX", "unrecognized character found", &mut error);
                 break;
             } 
 
-            //push numbers to num_build_list
+            // push numbers to num_build_list
             if character.is_digit(10) || character == '.' {
                 num_build_list.push(character);
             }
 
-            //creating and pushing number into stack
+            // creating and pushing number into stack
             if (character == ' ' || is_op(character)) && !num_build_list.is_empty() {
 
-                //syntax error if it will be invalid from extra decimal point
+                // syntax error if it will be invalid from extra decimal point
                 if num_build_list[num_build_list.len()-1] == '.' {
                     error_occurred("SYNTAX", "unexpected decimal point", &mut error);
                     break;
                 }
 
-                //collecting all of num_build_list and putting it into unstripped_num
+                // collecting all of num_build_list and putting it into unstripped_num
                 let unstripped_num: String = num_build_list.clone().into_iter().collect();
 
-                //syntax error if the number has too many decimals
+                // syntax error if the number has too many decimals
                 if unstripped_num.matches('.').count() > 1 {
                     error_occurred("SYNTAX", "too many decimals", &mut error);
                     break;
                 }
 
-                //cleaning and parsing into 64bit float
+                // cleaning and parsing into 64bit float
                 let number_into_stack: f64 = unstripped_num.parse().expect("error parsing number to f64");
 
-                //make negative (if its negative) then push to stack
+                // make negative (if its negative) then push to stack
                 if negative == true {
                     stack.push(0.0-number_into_stack);
                     negative = false;
                 } else {stack.push(number_into_stack);}
 
-                //clear num_build_list to prepare for the next number
+                // clear num_build_list to prepare for the next number
                 num_build_list.clear();
             } 
 
@@ -150,12 +150,12 @@ fn main() {
 
             if stack.len() >= 2 {
 
-                //match operation & run it
+                // match operation & run it
                 match operation {
                     '*' => {run_operation!(stack, *);}
                     '/' => {
                         
-                        //math error if there is division by zero
+                        // math error if there is division by zero
                         if stack[stack.len()-1] == 0.0 {
                             error_occurred("MATH", "div/0 -> undefined", &mut error);
                             break;
@@ -174,17 +174,17 @@ fn main() {
         //  print final answer and reset
         /////////////////////    /////////////////////
 
-        //check if there is more then 1 thing left in stack
+        // check if there is more then 1 thing left in stack
         if stack.len() > 1 && !error {
             error_occurred("SYNTAX", "too few operations", &mut error);
         }
 
-        //print out result if everything went well
+        // print out result if everything went well
         if !error && stack.len() > 0 { 
             println!("{}",stack[0]);
         }
 
-        //reset 
+        // reset 
         error = false;
         num_build_list.clear();
         stack.clear();
